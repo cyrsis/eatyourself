553c02aa775a0e1641850836529758b4d6516051
Victor <em3888@gmail.com>
2018-04-17 20:01:26 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# eatyourself
+
+Eat yourself is easier
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/AppStyle/AppStyle.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppStyle {
+  static final String  = "";
+
+  static get AppTheme => new ThemeData(
+    primarySwatch: Colors.blue,
+  );
+}
